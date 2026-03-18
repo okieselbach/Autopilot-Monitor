f74ab87d4bc78db7fad1141d7c6e138ccb0b6db8
Oliver Kieselbach <oliver.kieselbach@outlook.com>
2026-03-18 02:54:14 +0100

    navbar layout change #4

--- src/Web/autopilot-monitor-web/components/Navbar.tsx
+++ src/Web/autopilot-monitor-web/components/Navbar.tsx
@@ -231,7 +231,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-30">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="px-3">
         <div className="flex justify-between h-14">
           {/* Logo and Title */}
           <div className="flex items-center">
