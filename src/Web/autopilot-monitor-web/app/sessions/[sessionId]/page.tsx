5c45e072d266beaf2d6310f6eaee78ba5cbcf519
Oliver Kieselbach <oliver.kieselbach@outlook.com>
2026-03-20 21:57:52 +0100

    layout update

--- src/Web/autopilot-monitor-web/app/sessions/[sessionId]/page.tsx
+++ src/Web/autopilot-monitor-web/app/sessions/[sessionId]/page.tsx
@@ -982,7 +982,7 @@ export default function SessionDetailPage() {
         </div>
       </header>
 
-      <main className="py-6 px-4 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
           {/* Session Info Card */}
           {session && (
